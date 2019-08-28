979c4fd2424617b0823a0179a7e41e08a49ab080
suertang <suertang@163.com>
2019-08-28 18:35:32 +0800

    Update unlimitxiaoke.js

--- unlimitxiaoke.js
+++ unlimitxiaoke.js
@@ -1,32 +1,20 @@
+// ==UserScript==
 // @name         小可去转无限搜
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       suertang
-// @match        https://www.xiaokesoso.com/*
-// @match        https://www.quzhuanpan.com/download/*
+// @match        *://www.xiaokesoso.com/*
+// @match        *://www.quzhuanpan.com/download/*
 // @grant        none
 // ==/UserScript==
 
-
-
-
 (function() {
     'use strict';
-
-NodeList.prototype.remove=function(){
-    this.forEach((item)=>item.parentNode.removeChild(item));
-}
-
-    // Your code here...
-    document.querySelectorAll(".media-bottom> p:nth-child(1) > span:nth-child(4)").remove()
-    document.querySelectorAll(".media-bottom> p:nth-child(1) > span:nth-child(4)").remove()
+    // code here
     const ele = document.querySelector('button[data-downloadurl]');
     if(ele){
-
-        //const panurl = b64DecodeUnicode(ele.dataset.downloadurl);
-
-        var jumpUrl = "http://norefer.mimixiaoke.com/api/jump?target=" + ele.dataset.downloadurl;
+        const jumpUrl = "http://norefer.mimixiaoke.com/api/jump?target=" + ele.dataset.downloadurl;
         window.location.href = jumpUrl;
     }
 })();
