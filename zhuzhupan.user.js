08344d7bf6ca3b50ae5e6445a4ce4e516b949ed3
suertang <suertang@163.com>
2019-09-02 22:12:18 +0800

    Update zhuzhupan.user.js

--- zhuzhupan.user.js
+++ zhuzhupan.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         zhuzhupan
-// @namespace    http://woodytang.com/
-// @version      0.1
+// @namespace    http://tampermonkey.net/
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://www.zhuzhupan.com/search*
+// @updateURL    https://github.com/suertang/MMDigtal/raw/master/zhuzhupan.user.js
+// @downloadURL  https://github.com/suertang/MMDigtal/raw/master/zhuzhupan.user.js
 // @grant        none
 // ==/UserScript==
 
