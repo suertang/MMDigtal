c7b7992b0116c1607b317a15777475e49ca7af9f
suertang <suertang@gmail.com>
2020-06-21 15:52:00 +0800

    Update logic and fix show

--- JSBOX/1024pic.js
+++ JSBOX/1024pic.js
@@ -4,7 +4,7 @@
 */
 $cache.set("id", "16")
 $cache.set("pg", 1)
-var urlt = "https://cc.2tj4.icu/"
+var urlt = "https://cl.dc72.xyz/"
 
 var data = [{ "name": "达盖尔的旗帜", "id": "16" }, 
             { "name": "新时代的我们", "id": "8" }, ]
@@ -79,37 +79,49 @@ function getdata() {
     var pg = $cache.get("pg")
     $ui.loading(true)
     $http.get({
-        url: urlt + "thread0806.php?fid=" + id + "&page=" + pg,
+        url: urlt + "thread0806.php?fid=" + id + "&page=" + pg+"",
+        header: {
+                    'User-Agent': "mozilla/5.0 (iphone; cpu iphone os 11_0 like mac os x) applewebkit/604.1.38 (khtml, like gecko) version/11.0 mobile/15a372 safari/604.1"},
         handler: function (resp) {
             $ui.loading(false)
             var text = resp.data.replace(/\n|\s|\r/g, "")
-            console.log("text length before split",text.length)
+            //console.log(text)
+            //console.log("text length before split",text.length)
             //if (text.indexOf("普通主题") !== -1) {
             //console.log("找到普通主题")
-            if (text.indexOf("<trclass=\"tr2\">") !== -1) {
-                const para = text.split("<trclass=\"tr2\">")
+            if(text.indexOf("普通主題") !== -1) {
+                const para = text.split("普通主題")
                 text = para[para.length-1]
             }
-            console.log("text length after split",text.length)
-            var shu = text.match(/class="tr3t_onetac">(\S*?)<\/h3>/g)
+            //console.info(text)
+
+	
+            
+            //console.log("text length after split",text.length)
+            //var shu = text.match(/class="listt_one"(\S*?)<\/a>/g)
+            const reg = /(htm_data.*?)".*id="">(<.*?>)?(.*?)(<\/font>)?<\/a>+?/g
             var data
             if (pg == 1) {
-                data = []
+               data = []
             } else {
-                data = $("list").data
+              data = $("list").data
             }
-            for (let i in shu) {
-                var a = shu[i]
-                if (a.indexOf('href=') !== -1) {
-                    var txt = a.split("<h3>")[1]
-                    var mc = txt.match(/">(\S*?)<\/a>/)[1].replace(/<\S*?(\/?).*?>/g,"")
-                    var id = a.match(/href="(\S*?)"/)[1]
-                    //console.log(id);
-                    data.push({label:{text:mc},url:id})
-                }
+//            let matches = []
+            let match=reg.exec(resp.data)
+            while(match)
+            {
+              
+              data.push({label:{text:match[3]},url:match[1]})
+              //matches.push(match[3])
+              match=reg.exec(resp.data)
             }
+            console.log(data)
+            //let newmatch = resp.data.match()
+            
+                
+            
             $("list").data = data
-            console.info(data)
+            //console.info(data)
             $("list").endFetchingMore()
         }
     })
@@ -137,19 +149,22 @@ function isGif(imgurl){
     return !imgurl.toLowerCase().endsWith('gif')
 }
 */
-var genGallery = function(urls){
+var genGallery = function(urls,mc){
     //let html=''
     //urls=urls.filter(isGif)
     //console.log(urls)
-    var g=[]
+   
+    //var g=[]
+    let imgs=""
     for (let i of urls){
-        g.push(
-            {"ima":{"src":i}
-        })
-        //html += '<img src=' + i + ' /><br /><br />'
+        //g.push(
+        //    {"ima":{"src":i}
+        //})
+        imgs += '<img style="width:100%;height:auto" src=' + i + ' /><br /><br />'
     }
-    //return html;
-    return g;
+     let html=`<html><head><meta name=”viewport” content=”width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=1.0, user-scalable=yes” /><meta charset="UTF-8"><title>${mc}</title></head><body>${imgs}</body></html>`
+    return html;
+    //return g;
 }
 function geting(id, mc) {
     $ui.loading(true)
@@ -157,12 +172,23 @@ function geting(id, mc) {
         url: urlt +""+ id,
         handler: function (resp) {
             $ui.loading(false)
-            var text = resp.data.match(/<div class="tpc_content do_not_catch">[\s\S]*?<tr class="tr1">/)
-            const imgs = getimgsrc(text)//.match(regimg);
-
-            console.log(imgs);
-           
-            const html = genGallery(imgs)
+            console.log(resp.data)
+            const attr = resp.data.match(/img\[(.*?)\]/)[1]
+            console.log(attr)
+            const imgReg = new RegExp(attr+"='(.*?)'","g")
+            
+            var text = resp.data//.match(/<div class="tpc_cont">[\s\S]*?$/)
+            //console.info(text)
+            //const imgs = getimgsrc(text)//.match(regimg);
+            let imgs=[]
+           let m=imgReg.exec(text)
+           while(m)
+           {
+             
+             imgs.push(m[1])
+             m=imgReg.exec(text)
+           }
+            const html = genGallery(imgs,mc)
             console.info(html)
             $ui.push({
               props:{
@@ -171,28 +197,14 @@ function geting(id, mc) {
                 },
             views:[
                 {
-                    type: "list",
+                    type: "web",
                     
                     props:{
                       id:'ga',
-                      data:html,
-                      rowHeight:600,
-                      template:[{                        
-                            type: "image",
-                            props:{
-                                id:"ima"
-                            },
-                            layout: function(make, view) {
-                              make.center.equalTo(view.super)
-                              make.size.equalTo($size(600, 480))
-                            }
-                        }]
-                    },
-                    layout: function(make,view){
-                        make.top.equalTo($("meun").bottom)
-                        make.left.right.inset(10)
-                        make.height.equalTo(view.super.height)
-                    }
+                      html:html
+                      },
+                      
+                 layout: $layout.fill
                 }]      
             
         })
