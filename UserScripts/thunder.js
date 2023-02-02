5113402715d0299c66d75c37f4a2ac786257e6d7
Tang Zhongji <suertang@gmail.com>
2023-02-02 18:29:35 +0800

    test with console log

--- UserScripts/thunder.js
+++ UserScripts/thunder.js
@@ -1,16 +1,18 @@
 
 var obj = JSON.parse($response.body);
+console.log($response.body)
 var nor = {
         "status" : "STATUS_OK",
         "message" : "正常资源",
         "title" : ""
-      }
+      };
 // console.log(body.files)
 // obj.files.map((x) => x.audit = Object.create(nor))
 
 for(var item of obj.files){
-  item.audit = Object.create(nor)
+  item.audit = Object.create(nor);
 }
 
 body = JSON.stringify(obj);
+console.log(body)
 $done({body});
